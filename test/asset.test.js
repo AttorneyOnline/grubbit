e598fa1b3232ae8bf2c8110a3ef0da3268a2e93f
oldmud0 <oldmud0@users.noreply.github.com>
2020-01-08 01:06:08 -0600

    Extract class

--- test/asset.test.js
+++ test/asset.test.js
@@ -1,14 +1,16 @@
-import { getAsset } from '../lib/asset';
+import { AssetDB } from '../lib/asset';
 
 const webFetch = window.fetch;
 import fetchMock from 'fetch-mock';
 
-const BASE = 'https://assets.animatedchatroom.net';
+const BASE = 'https://mock-assets.website';
+const BROKEN = 'https://broken.website';
 fetchMock.mock(`begin:${BASE}`,
   async (url, options) => {
     const resource = url.substring(BASE.length + 1);
     return await webFetch(`base/test/fixtures/${resource}`, options);
-  });
+  })
+  .mock(`begin:${BROKEN}`, { status: 404 });
 
 describe('environment', () => {
   it('should fetch a sample fixture', async () => {
@@ -18,15 +20,21 @@ describe('environment', () => {
 });
 
 describe('asset system', () => {
+  let assets;
+  beforeAll(async () => {
+    assets = new AssetDB('assets', [BASE, BROKEN]);
+    assets.clearAll();
+  });
+
   it('downloads a remote resource', async () => {
-    const res = await getAsset(`${BASE}/banana.gif`, true);
+    const res = await assets.getAsset(`${BASE}/banana.gif`, true);
     expect(res.type).toBe('image/gif');
   });
 
   it('downloads an asset', async () => {
     const pkg = '3e12c59c966cf442008eb43053183f15d3480499';
     const file = 'd3e61a0597ff455ce63299993882318073371f81';
-    const res = await getAsset(`@${pkg}/${file}`, true);
+    const res = await assets.getAsset(`@${pkg}/${file}`, true);
     expect(res.type).toBe('image/gif');
-  })
+  });
 });
